06913d47591252152f5226b1ac5dc1f7f71d9ebc
Arthur <arthurrochadsg@gmail.com>
2023-05-05 09:18:53 -0300

    ds

--- pages/TelaC.jsx
+++ pages/TelaC.jsx
@@ -2,14 +2,24 @@ import { View } from "react-native"
 
 const telaC = () => {
     return(
-        <View>
+        <View style={styles.container}>
             <Titulo/>
+            <View style={styles.footer}>
+           
+            </View>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        backgroundColor:'black',
     
+    },
+    footer:{
+        
+    }
 })
 
 export default telaC
