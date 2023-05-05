19b58f94f0791349165501a119075083debb5785
Anselmo <anselmohenrique191@gmail.com>
2023-05-05 09:26:30 -0300

    anslemo

---
+++ Draw/pages/TelaC.jsx
@@ -0,0 +1,15 @@
+import { View,Text,Pressable,StyleSheet } from "react-native"
+
+const TelaC = () => {
+    return(
+        <View>
+            <Titulo/>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    
+})
+
+export default TelaC
