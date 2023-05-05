033d27482472f006633d04ac22ef34ef49ea472d
Arthur <arthurrochadsg@gmail.com>
2023-05-05 19:30:49 -0300

    dksd

--- react-navigation/TabsNavegationBottom/app.js
+++ react-navigation/TabsNavegationBottom/app.js
@@ -1,9 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-export default function App() {
+const Tab = createBottomTabNavigator();
+
+function HomeScreen(){
   return (
-    <View style={styles.container}>
+<View style={styles.container}>
       <StatusBar style="auto" />
       <View style={styles.footer}>
 
@@ -23,3 +27,23 @@ const styles = StyleSheet.create({
     height:'10%',
   }
 });
+  
+
+
+function MyTabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Settings" component={SettingsScreen} />
+    </Tab.Navigator>
+  );
+}
+
+export default function App() {
+  return (
+ <NavigationContainer>
+  <MyTabs/>
+ </NavigationContainer>
+   
+  );
+}
